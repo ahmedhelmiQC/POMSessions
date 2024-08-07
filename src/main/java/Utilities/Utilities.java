7c256a13e18242eaac1710d8258dc1a2802f5598
ahmed helmiQA <ahmed.systemtester@gmail.com>
2024-08-07 22:43:42 +0300

    Allure Report

--- src/main/java/Utilities/Utilities.java
+++ src/main/java/Utilities/Utilities.java
@@ -1,20 +1,28 @@
 package Utilities;
 
+import io.qameta.allure.Allure;
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.time.Duration;
 
 public class Utilities {
+    private static String Screenshots_Path = "test-outputs/screenshot/";
     /**
      * Utility function for finding , clicking an elements after waiting to be clicking
      * @param driver
      * @param locater object
      *
-
      */
 
     //ToDo:  clicking on element
@@ -30,6 +38,12 @@ public class Utilities {
 
     //ToDo: selecting from drop down
     //ToDo: Tacking screenshots
+    public static void takescreenshot(WebDriver driver , String screenshotname) throws IOException {
+        File screenSrc = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE); // take screenshot
+        File screenDest= new File(Screenshots_Path+screenshotname+".png"); // where save the screen
+        FileUtils.copyFile(screenSrc,screenDest);
+        Allure.addAttachment(screenshotname, Files.newInputStream(Path.of(screenDest.getPath())));
+    }
 
     //ToDo: uploading files using robot
 
--- src/test/java/TC01_LoginTest.java
+++ src/test/java/TC01_LoginTest.java
@@ -2,6 +2,7 @@ import Pages.P01_LoginPage;
 import Utilities.DataUtili;
 import Utilities.Utilities;
 import com.github.javafaker.Faker;
+import io.qameta.allure.*;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.testng.Assert;
@@ -23,15 +24,32 @@ public class TC01_LoginTest {
         driver.get(DataUtili.getpropertyvalue("environments","LOGIN_URL"));
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
     }
+         /// Allure annotations ///
+    @Description ("This test case verify that user logged in successfully")
+    @Severity(SeverityLevel.CRITICAL)
+    @Owner("Ahmed")
+    @Link(name="Business Document" , url ="https://www.google.com")
+    @Issue("https://github.com/ahmedhelmiQC")
+    @TmsLink("https:jira.com/zephyr/TC50")
+    @Epic("Web panel")
+    @Feature("Login")
+    @Story("Valid login on web")
+    ///
     @Test
     public void validLoginTC() throws IOException {
       new P01_LoginPage(driver);
-        new P01_LoginPage(driver).enterUsernameText(DataUtili.getJsonData("validloginData","username"))
-                .enterPassword(DataUtili.getJsonData("validloginData","password"))
+        entervalidusername();
+        new P01_LoginPage(driver).enterPassword(DataUtili.getJsonData("validloginData","password"))
                     .clickLoginButton();
+        Utilities.takescreenshot(driver,"first screenshot");
 
        Assert.assertEquals(driver.getCurrentUrl(),DataUtili.getpropertyvalue("environments","HOME_URL"));
     }
+    @Step("step 1")
+    public void entervalidusername() throws FileNotFoundException {
+        new P01_LoginPage(driver).enterUsernameText(DataUtili.getJsonData("validloginData","username"));
+    }
+
     @Test
     public void invalidLoginTC() throws IOException  ///// Java Faker
     {
@@ -40,7 +58,7 @@ public class TC01_LoginTest {
                 .enterPassword(DataUtili.getJsonData("validloginData","password"))
                 .clickLoginButton();
 
-        Assert.assertNotEquals(driver.getCurrentUrl(),DataUtili.getpropertyvalue("environments","HOME_URL"));
+        Assert.assertEquals(driver.getCurrentUrl(),DataUtili.getpropertyvalue("environments","HOME_URL"));
     }
     @AfterMethod
     public void Quite(){
